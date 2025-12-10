904d145bdf226750e0562860dc8e160d3abdebb8
Katy Wells <katy.wells@verizon.net>
2025-12-10 15:25:20 -0500

    feat(components): set up all component files and folders, create basic function placehoders, import and export files

---
+++ client/src/Components/pantry/PantryItem.tsx
@@ -0,0 +1,13 @@
+import styles from './pantry.css'
+
+import React from 'react'
+
+const PantryItem = () => {
+
+    
+  return (
+    <div>PantryItem</div>
+  )
+}
+
+export default PantryItem
---
+++ client/src/Components/pantry/PantryItemContainer.tsx
@@ -0,0 +1,13 @@
+import styles from './pantry.css'
+
+import React from 'react'
+
+const PantryItemContainer = () => {
+
+    
+  return (
+    <div>PantryItemContainer</div>
+  )
+}
+
+export default PantryItemContainer
